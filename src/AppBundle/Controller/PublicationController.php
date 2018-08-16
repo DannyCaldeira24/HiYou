f0e3e005e79036c3b777e20e66a40e9a8c86277d
Danny Caldeira <dannyelportu2013@gmail.com>
2018-08-16 18:05:10 -0400

    Configurando el routing y los controladores

--- src/AppBundle/Controller/PublicationController.php
+++ src/AppBundle/Controller/PublicationController.php
@@ -1,18 +1,14 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace AppBundle\Controller;
 
-/**
- * Description of PublicationController
- *
- * @author DannyCaldeira
- */
-class PublicationController {
-    //put your code here
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PublicationController extends Controller{
+    public function indexAction(Request $request){
+        echo "Acción index publicatión";
+        die();
+    }
 }
--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -1,18 +1,15 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace AppBundle\Controller;
 
-/**
- * Description of UserController
- *
- * @author DannyCaldeira
- */
-class UserController {
-    //put your code here
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends Controller
+{
+    public function loginAction(Request $request){
+        echo "Acción login";
+        die();
+    }
 }

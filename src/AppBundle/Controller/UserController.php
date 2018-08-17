2fdef683c72a8e2ff3d913c87e9a9fa7aaad4647
Danny Caldeira <dannyelportu2013@gmail.com>
2018-08-17 00:16:19 -0400

    Agregando formulario de registro

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -6,9 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+use BackendBundle\Entity\User;
+use AppBundle\Form\RegisterType;
+
 class UserController extends Controller
 {
     public function loginAction(Request $request){
         return $this->render('AppBundle:User:login.html.twig', array("titulo"=>"Login"));
+    }
+    
+    public function registerAction(Request $request){
+        $user=new User();
+        $form=$this->createForm(RegisterType::class,$user);
+        return $this->render('AppBundle:User:register.html.twig', array("form"=>$form->createView()));
     }
 }
